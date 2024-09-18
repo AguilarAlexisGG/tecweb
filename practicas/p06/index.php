c26d62a787854a15d7c8ab7690c3b973c3d3c748
AguilarAlexis <alexis.aguilarga@alumno.buap.mx>
2024-09-18 15:59:01 -0600

    p06_1

---
+++ practicas/p06/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica 4</title>
+</head>
+<body>
+    <h2>Ejercicio 1</h2>
+    <p>Escribir programa para comprobar si un número es un múltiplo de 5 y 7</p>
+    <?php
+        include 'src/funciones.php';
+        if(isset($_GET['numero']))
+        {
+            $num = $_GET['numero'];
+            if (esMultiplo($num))
+            {
+                echo '<h3>R= El número '.$num.' SÍ es múltiplo de 5 y 7.</h3>';
+            }
+            else
+            {
+                echo '<h3>R= El número '.$num.' NO es múltiplo de 5 y 7.</h3>';
+            }
+        }
+    ?>
+    
+    <h2>Ejemplo de POST</h2>
+    <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
+        Name: <input type="text" name="name"><br>
+        E-mail: <input type="text" name="email"><br>
+        <input type="submit">
+    </form>
+    <br>
+    <?php
+        if(isset($_POST["name"]) && isset($_POST["email"]))
+        {
+            echo $_POST["name"];
+            echo '<br>';
+            echo $_POST["email"];
+        }
+    ?>
+    
+    
+</body>
+</html>
---
+++ practicas/p06/src/funciones.php
@@ -0,0 +1,7 @@
+<?php
+function esMultiplo($num) {
+    return ($num % 5 == 0 && $num % 7 == 0);
+}
+
+
+?>
