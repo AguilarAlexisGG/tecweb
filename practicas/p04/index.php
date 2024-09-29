583d7a1a9b99ee4c5b3d640f5290fa9e2887b0b4
AguilarAlexis <alexis.aguilarga@alumno.buap.mx>
2024-09-28 23:53:24 -0600

    Validación de XHTML

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <title>Práctica 3</title>
 </head>
 <body>
@@ -18,7 +18,7 @@
         $_element1;
         //$house*5;     // InvalidaSSS
 
-        echo '<br><h4><p>1. Determina cuál de las siguientes variables son válidas y explica por qué: <p/></h4>'; 
+        echo '<br>'.'<h4>1. Determina cuál de las siguientes variables son válidas y explica por qué:</h4>'; 
     
         echo '<ul>';
         echo '<li>$_myvar es válida porque inicia con guión bajo.</li>';
@@ -30,7 +30,7 @@
         echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
         echo '</ul>';
 
-        echo '<br><h4><p>2. Proporcionar los valores de $a, $b, $c como sigue:<p/></h4>';
+        echo '<br><h4>2. Proporcionar los valores de $a, $b, $c como sigue:</h4>';
         $a = "ManejadorSQL";
         $b = 'MySQL';
         $c = &$a;
@@ -39,7 +39,7 @@
         echo $b.'<br>';
         echo $c;
 
-        echo '<br><h4><p>b. Agrega al código actual las siguientes asignaciones:<p/></h4>';
+        echo '<br><h4>b. Agrega al código actual las siguientes asignaciones:</h4>';
         
         $a = "PHP server";
         $b = &$a;
@@ -47,8 +47,8 @@
         echo $a.'<br>';
         echo $b.'<br>';
 
-        echo '<br><h4><p>d. Describe en y muestra en la página obtenida qué ocurrió en el 
-        segundo bloque de asignaciones<p/></h4>';
+        echo '<br><h4>d. Describe en y muestra en la página obtenida qué ocurrió en el 
+        segundo bloque de asignaciones</h4>';
 
         echo '<p>Respuesta: Asignamos otro valor a la varible 
             $a y despues le asignamos a $b el valor de la 
@@ -58,8 +58,8 @@
         unset($b);
         unset($c);
 
-        echo '<br><h4><p>3. Muestra el contenido de cada variable inmediatamente después de cada asignación,
-        verificar la evolución del tipo de estas variables (imprime todos los componentes de los arreglo): <p/></h4>';
+        echo '<br><h4>3. Muestra el contenido de cada variable inmediatamente después de cada asignación,
+        verificar la evolución del tipo de estas variables (imprime todos los componentes de los arreglo):</h4>';
         $a = "PHP5";
             echo $a.'<br>';
         $z[] = &$a;
@@ -77,8 +77,8 @@
             print_r($z);
             echo '<br>';
 
-        echo '<br><h4><p>4. Lee y muestra los valores de las variables del ejercicio anterior, pero ahora con la ayuda de
-        la matriz $GLOBALS o del modificador global de PHP.<p/></h4>';
+        echo '<br><h4>4. Lee y muestra los valores de las variables del ejercicio anterior, pero ahora con la ayuda de
+        la matriz $GLOBALS o del modificador global de PHP.</h4>';
         function mostrar(){
             echo '$a en el ambito global: '.$GLOBALS['a'].'<br>';
             echo '$b en el ambito global: '.$GLOBALS['b'].'<br>';
@@ -89,7 +89,7 @@
         }        
         mostrar();
 
-        echo '<br><h4><p>5. Dar el valor de las variables $a, $b, $c al final del siguiente script: <p/></h4>';
+        echo '<br><h4>5. Dar el valor de las variables $a, $b, $c al final del siguiente script:</h4>';
        
         $a = "7 personas";
         $b = (integer) $a;
@@ -105,8 +105,8 @@
         unset($c);
         unset($z);
 
-        echo '<br><h4><p>6. Dar y comprobar el valor booleano de las variables $a, $b, $c, $d, $e y $f y 
-        muéstralas usando la función var_dump(<datos>). <p/></h4>';
+        echo '<br><h4>6. Dar y comprobar el valor booleano de las variables $a, $b, $c, $d, $e y $f y 
+        muéstralas usando la función var_dump(<datos>).</h4>';
         
         $a = "0";
         $b = "TRUE";
@@ -122,8 +122,8 @@
         var_dump($e); echo '<br>';
         var_dump($f); echo '<br>';
        
-        echo '<p><h4>Después investiga una función de PHP que permita transformar el valor booleano de $c y $e 
-        en uno que se pueda mostrar con un echo: </h6><p/>';
+        echo '<h4>Después investiga una función de PHP que permita transformar el valor booleano de $c y $e 
+        en uno que se pueda mostrar con un echo: </h4>';
 
         //echo (bool) $a ? 'true' : 'false'; echo '<br>';
         //echo (bool) $b ? 'true' : 'false'; echo '<br>';
@@ -132,17 +132,21 @@
         echo (bool) $e ? 'true' : 'false'; echo '<br>';
         //echo (bool) $f ? 'true' : 'false'; echo '<br>';
 
-        echo '<p><h4> 7. Usando la variable predefinida $_SERVER, determina lo siguiente:</h6><p/>';
-        echo '<p><h4>a. La versión de Apache y PHP.</h6><p/>';
+        echo '<h4> 7. Usando la variable predefinida $_SERVER, determina lo siguiente:</h4>';
+        echo '<h4>a. La versión de Apache y PHP.</h4>';
 
         echo $_SERVER['SERVER_SOFTWARE'];
         
-        echo '<p><h4>b. El nombre del sistema operativo (servidor).</h6><p/>';
+        echo '<h4>b. El nombre del sistema operativo (servidor).</h4>';
         echo PHP_OS;
 
-        echo '<p><h4>c. El idioma del navegador (cliente).</h6><p/>';
+        echo '<h4>c. El idioma del navegador (cliente).</h4>';
         echo $_SERVER['HTTP_ACCEPT_LANGUAGE'];
 
     ?>
+    <p>
+        <a href="https://validator.w3.org/markup/check?uri=referer"><img
+        src="https://www.w3.org/Icons/valid-xhtml11" alt="XHTML 1.1 válido" height="31" width="88" /></a>
+    </p>
 </body>
 </html>
