f959eb287c9dd6cabc39fceb1fd6dd1f2737f8bd
AguilarAlexis <alexis.aguilarga@alumno.buap.mx>
2024-10-08 18:31:33 -0600

    a04_Inicio

---
+++ actividades/a04/src/main.js
@@ -0,0 +1,11 @@
+/*
+function getDatos(){
+    var nombre = prompt('Nombre: ', '');
+    var edad = prompt('Edad: ', 0);
+
+    var div1 = document.getElementById('nombre');
+    div1.innerHTML = '<h3>Nombre: ' + nombre + '</h3>';
+    var div2 = document.getElementById('edad');
+    div2.innerHTML = '<h3>Edad: ' + edad + '</h3>';
+}
+*/
