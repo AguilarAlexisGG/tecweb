626a28f93c81ac25a1a968db684a34468e2f41fb
AguilarAlexis <alexis.aguilarga@alumno.buap.mx>
2024-11-10 09:33:20 -0600

    p06_Iniciada

---
+++ actividades/a06/product_app/backend/database.php
@@ -0,0 +1,15 @@
+<?php
+    $conexion = @mysqli_connect(
+        'localhost',
+        'root',
+        'sapo123',
+        'marketzone'
+    );
+
+    /**
+     * NOTA: si la conexión falló $conexion contendrá false
+     **/
+    if(!$conexion) {
+        die('¡Base de datos NO conextada!');
+    }
+?>
