d0c95e7f9f1f74868430bd2f867b3638dd2d3a66
AguilarAlexis <alexis.aguilarga@alumno.buap.mx>
2024-11-10 18:07:53 -0600

    p06_Completada

--- actividades/a06/product_app/app.js
+++ actividades/a06/product_app/app.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
                                 <td><a href="#" class="product-item">${producto.nombre}</a></td>
                                 <td><ul>${descripcion}</ul></td>
                                 <td>
-                                    <button class="product-delete btn btn-danger" onclick="eliminarProducto()">
+                                    <button class="product-delete btn btn-danger">
                                         Eliminar
                                     </button>
                                 </td>
@@ -56,6 +56,79 @@ $(document).ready(function(){
                 }
             }
         });
+    }
+
+    function validar(JSON){
+        let template_bar = '';
+        let esValido = true;
+    //NOMBRE
+        //let nombre = document.getElementById("nombre").value.trim();
+        if(!JSON.nombre || JSON.nombre.length == 0){
+            esValido = false;
+            template_bar += `
+                <li style="list-style: none;">message: Nombre Obligatortio</li>
+            `;
+        }
+        else if(JSON.nombre.length > 100){
+            esValido = false;
+        }
+    //MARCA
+        //let marca = document.getElementById("marca").value.trim();
+        let marcas = ["DeWalt", "IUSA", "BELLOTA", "TRUPER", "PHILLIPS", "FIERO", "COFLEX"];
+        if(!JSON.marca || JSON.marca.length == 0){
+            esValido = false;
+            template_bar += `
+                <li style="list-style: none;">message: Marca Obligatortia</li>
+            `;
+        }else if(!marcas.includes(JSON.marca)){
+            
+            esValido = false;
+        }
+    //MODELO
+        //let modelo = document.getElementById("modelo").value.trim();
+        //let patronModelo = /^[a-zA-Z0-9\s]+$/;
+        if(!JSON.modelo||  JSON.modelo.length == 0){
+            esValido = false;
+            template_bar += `
+                <li style="list-style: none;">message: Modelo Obligatortio</li>
+            `;
+        }else if(!/^[a-zA-Z0-9\s]+$/.test(JSON.modelo) || JSON.modelo.length > 25){
+            esValido = false;
+        }
+    //PRECIO
+        //let precio = parseFloat(document.getElementById("precio").value.trim());
+        if(JSON.precio == ''){
+            esValido = false;
+            template_bar += `
+            <li style="list-style: none;">message: Precio Obligatortio</li>
+            `;
+        }
+        else if(JSON.precio <= 99.99){
+            esValido = false;
+        } 
+    //DETALLES
+        //let detalles = document.getElementById("detalles").value.trim();
+        if(JSON.detalles && JSON.detalles.length > 250){
+            esValido = false;
+        }
+    //UNIDADES
+        //let unidades = parseFloat(document.getElementById("unidades").value);
+        if(JSON.unidades == ''){
+            esValido = false;
+            template_bar += `
+                <li style="list-style: none;">message: Unidades Obligatortio</li>
+            `;
+        }
+        else if(JSON.unidades < 0){
+            esValido = false;
+        }
+    //IMAGEN
+    if (!JSON.imagen || JSON.imagen.length == 0) {
+        JSON.imagen = "img/imagen.png";
+    }
+        $('#product-result').show();
+        $('#container').html(template_bar);
+        return esValido;
     }
 
     $('#search').keyup(function() {
@@ -122,15 +195,27 @@ $(document).ready(function(){
         e.preventDefault();
 
         // SE CONVIERTE EL JSON DE STRING A OBJETO
-        let postData = JSON.parse( $('#description').val() );
+        let postData = {
+            id: $('#productId').val(),
+            nombre: $('#name').val(),
+            marca: $('#marca').val(),
+            modelo: $('#modelo').val(),
+            precio: $('#precio').val(),
+            detalles: $('#detalles').val(),
+            unidades: $('#unidades').val(),
+            imagen: $('#imagen').val()
+        };
         // SE AGREGA AL JSON EL NOMBRE DEL PRODUCTO
-        postData['nombre'] = $('#name').val();
-        postData['id'] = $('#productId').val();
-
+        //**/postData['nombre'] = $('#name').val();
+        //**/postData['id'] = $('#productId').val();
         /**
          * AQUÍ DEBES AGREGAR LAS VALIDACIONES DE LOS DATOS EN EL JSON
          * --> EN CASO DE NO HABER ERRORES, SE ENVIAR EL PRODUCTO A AGREGAR
          **/
+        console.log(postData);
+        if(!validar(postData)){
+            return;
+        }
 
         const url = edit === false ? './backend/product-add.php' : './backend/product-edit.php';
         
@@ -145,9 +230,14 @@ $(document).ready(function(){
                         <li style="list-style: none;">message: ${respuesta.message}</li>
                     `;
             // SE REINICIA EL FORMULARIO
-            $('#name').val('');
-            $('#description').val(JsonString);
-            // SE HACE VISIBLE LA BARRA DE ESTADO
+            $('#name').val(''); // SE LIMPIA EL CAMPO DE NOMBRE
+            $('#marca').val(''); // SE LIMPIA EL CAMPO DE MARCA
+            $('#modelo').val(''); // SE LIMPIA EL CAMPO DE MODELO
+            $('#precio').val(''); // SE LIMPIA EL CAMPO DE PRECIO
+            $('#detalles').val(''); // SE LIMPIA EL CAMPO DE DETALLES
+            $('#unidades').val(''); // SE LIMPIA EL CAMPO DE UNIDADES
+            $('#imagen').val(''); // SE LIMPIA EL CAMPO DE IMAGEN
+            $('#productId').val(''); // SE LIMPIA EL CAMPO DE ID
             $('#product-result').show();
             // SE INSERTA LA PLANTILLA PARA LA BARRA DE ESTADO
             $('#container').html(template_bar);
@@ -179,18 +269,175 @@ $(document).ready(function(){
             $('#name').val(product.nombre);
             // EL ID SE INSERTA EN UN CAMPO OCULTO PARA USARLO DESPUÉS PARA LA ACTUALIZACIÓN
             $('#productId').val(product.id);
+            $('#marca').val(product.marca);
+            $('#modelo').val(product.modelo);
+            $('#precio').val(product.precio);
+            $('#detalles').val(product.detalles);
+            $('#unidades').val(product.unidades);
+            $('#imagen').val(product.imagen);
+
             // SE ELIMINA nombre, eliminado E id PARA PODER MOSTRAR EL JSON EN EL <textarea>
-            delete(product.nombre);
-            delete(product.eliminado);
-            delete(product.id);
+            //delete(product.nombre);
+            //delete(product.eliminado);
+            //delete(product.id);
             // SE CONVIERTE EL OBJETO JSON EN STRING
-            let JsonString = JSON.stringify(product,null,2);
+            //let JsonString = JSON.stringify(product,null,2);
             // SE MUESTRA STRING EN EL <textarea>
-            $('#description').val(JsonString);
+            //$('#description').val(JsonString);
             
             // SE PONE LA BANDERA DE EDICIÓN EN true
             edit = true;
         });
         e.preventDefault();
-    });    
+    });  
+    
+    $('#name').keyup(function(e) {
+        e.preventDefault(); // Prevenir el comportamiento por defecto del formulario
+        var name = $('#name').val();
+        //console.log(name);
+        $.ajax({
+            url: './backend/product-singleByName.php',
+            data: {name: name},
+            type: 'GET',
+            success: function(response){
+                let productos = JSON.parse(response);
+                let template_bar = '';
+                if (Object.keys(productos).length > 0) {
+                    template_bar += `<li>El nombre ya existe en la base de datos</li>`;
+                } else {
+                    template_bar += `<li>Nombre valido</li>`;
+                }
+                $('#product-result').show();
+                $('#container').html(template_bar);
+            }
+        });
+    });
+    
 });
+
+function validarNombre(){
+    let template_bar = '';
+    let nombre = document.getElementById("name").value.trim();
+    document.getElementById("error-name").textContent = "";
+    if(nombre == ""){
+        template_bar += `
+            <li style="list-style: none;">status: Error</li>
+            <li style="list-style: none;">message: Nombre Obligatorio</li>
+        `;
+        document.getElementById("error-name").innerHTML = "<p>Nombre Obligatorio</p>";
+
+    }
+    else if(nombre.length > 100){
+        template_bar += `
+            <li style="list-style: none;">status: Error</li>
+            <li style="list-style: none;">message: Nombre muy largo</li>
+        `;
+        document.getElementById("error-nombre").innerHTML = "<p>Nombre muy largo</p>";
+    }
+    $('#product-result').show();
+    $("#container").html(template_bar);
+}
+
+function validarMarca(){
+    let template_bar = '';
+    let marca = document.getElementById("marca").value.trim();
+    document.getElementById("error-marca").textContent = "";
+    if(marca == ""){
+        template_bar += `
+        <li style="list-style: none;">status: Error</li>
+        <li style="list-style: none;">message: Marca Obligatortio</li>
+        `;
+        document.getElementById("error-marca").innerHTML = "<p>Marca Obligatortio</p>";
+    }
+    $('#product-result').show();
+    $("#container").html(template_bar);
+}
+
+function validarModelo(){
+    let template_bar = '';
+    let modelo = document.getElementById("modelo").value.trim();
+    document.getElementById("error-modelo").textContent = "";
+    let patronModelo = /^[a-zA-Z0-9\s]+$/;
+    if(modelo === "" || modelo.length > 25 || !patronModelo.test(modelo)){
+        template_bar += `
+        <li style="list-style: none;">status: Error</li>
+        <li style="list-style: none;">message: El modelo es obligatorio, debe ser alfanumérico y maximo 25 caracteres.</li>
+        `;
+        document.getElementById("error-modelo").innerHTML = "<p>El modelo es obligatorio, debe ser alfanumérico y maximo 25 caracteres.</p>";
+
+    }
+    $('#product-result').show();
+    $("#container").html(template_bar);
+}
+
+function validarPrecio(){
+    let template_bar = '';
+    let precio = parseFloat(document.getElementById("precio").value.trim());
+    document.getElementById("error-precio").textContent = "";
+    if(isNaN(precio)){
+        template_bar += `
+        <li style="list-style: none;">status: Error</li>
+        <li style="list-style: none;">message: Precio Obligatortio</li>
+        `;
+        document.getElementById("error-precio").innerHTML = "<p>Precio Obligatortio</p>";
+
+    }
+    else if(precio <= 99.99){
+        template_bar += `
+        <li style="list-style: none;">status: Error</li>
+        <li style="list-style: none;">message: Debe ser mayor a 99.9</li>
+        `;
+        document.getElementById("error-precio").innerHTML = "<p>Debe ser mayor a 99.99</p>";
+
+    } 
+    $('#product-result').show();
+    $("#container").html(template_bar);
+}
+function validarDetalles(){
+    let template_bar = '';
+    let detalles = document.getElementById("detalles").value.trim();
+    document.getElementById("error-detalles").textContent = "";
+    if(detalles.length > 250){
+        template_bar += `
+        <li style="list-style: none;">status: Error</li>
+        <li style="list-style: none;">message: Debe tener 250 caracteres o menos</li>
+        `;
+        document.getElementById("error-detalles").innerHTML = "<p>Debe tener 250 caracteres o menos.</p>";
+    }
+    $('#product-result').show();
+    $("#container").html(template_bar);
+}
+function validarUnidades(){
+    let template_bar = '';
+    let unidades = parseFloat(document.getElementById("unidades").value);
+    document.getElementById("error-unidades").textContent = "";
+    if(isNaN(unidades)){
+        template_bar += `
+        <li style="list-style: none;">status: Error</li>
+        <li style="list-style: none;">message: Unidades Obligatortio</li>
+        `;
+        document.getElementById("error-unidades").innerHTML = "<p>Unidades Obligatortio</p>";
+
+    }
+    else if(unidades < 0){
+        template_bar += `
+        <li style="list-style: none;">status: Error</li>
+        <li style="list-style: none;">message: Debe ser mayor o igual a 0</li>
+        `;
+        document.getElementById("error-unidades").innerHTML = "<p>Debe ser mayor o igual a 0</p>";
+    } 
+    $('#product-result').show();
+    $("#container").html(template_bar);
+}
+function validarImagen(){
+    let template_bar = '';
+    let imagen = document.getElementById("imagen").value.trim();
+    if (imagen == "") {
+        template_bar += `
+        <li style="list-style: none;">status: Advertencia</li>
+        <li style="list-style: none;">message: Imagen por defaul</li>
+        `;
+    }
+    $('#product-result').show();
+    $("#container").html(template_bar);
+}
---
+++ actividades/a06/product_app/backend/product-singleByName.php
@@ -0,0 +1,29 @@
+<?php
+    include_once __DIR__.'/database.php';
+
+    // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
+    $data = array();
+
+    if( isset($_GET['name']) ) {
+        $name = $_GET['name'];
+        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
+        if ( $result = $conexion->query("SELECT * FROM productos WHERE nombre = '{$name}' AND eliminado = 0") ) {
+            // SE OBTIENEN LOS RESULTADOS
+            $row = $result->fetch_assoc();
+
+            if(!is_null($row)) {
+                // SE CODIFICAN A UTF-8 LOS DATOS Y SE MAPEAN AL ARREGLO DE RESPUESTA
+                foreach($row as $key => $value) {
+                    $data[$key] = utf8_encode($value);
+                }
+            }
+            $result->free();
+        } else {
+            die('Query Error: '.mysqli_error($conexion));
+        }
+        $conexion->close();
+    }
+
+    // SE HACE LA CONVERSIÓN DE ARRAY A JSON
+    echo json_encode($data, JSON_PRETTY_PRINT);
+?>
